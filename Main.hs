c58e5b5a1ecd2d79802462f8c5df8ad092c3d0ed
Paul Graphov <graphov@gmail.com>
2013-03-26 10:34:04 +0400

    symlink-safe cleanup

--- Main.hs
+++ Main.hs
@@ -1,23 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Prelude hiding (FilePath)
+
+import Control.Applicative
 import Control.Monad
 import Control.Exception
 
+import Data.String (fromString)
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
 import System.Environment
 import System.Process
-import System.Directory
+
+import Filesystem
+import Filesystem.Path
+import Filesystem.Path.CurrentOS
 
 import System.Posix.Process
 
+showPath :: FilePath -> T.Text
+showPath = either id id . toText
+
 mkTempDir :: IO FilePath
 mkTempDir = do
-  tmp <- (("tmp_dir_" ++) . show) `liftM` getProcessID
-  createDirectory tmp
+  tmp <- fromString . ("tmp_dir_" ++) . show <$> getProcessID
+  createDirectory False tmp
   return tmp
 
-hello :: FilePath -> IO ()
-hello f = print f
+--      file       temp
+hello :: FilePath -> FilePath -> IO ()
+hello f t = do
+  T.putStrLn $ "Temp path: " `T.append` showPath t
+  T.putStrLn $ "File: " `T.append` showPath f
 
 main :: IO ()
 main = do
-  bracket mkTempDir removeDirectoryRecursive hello
+  file <- head <$> getArgs
+  bracket mkTempDir removeTree (hello $ fromString file)
